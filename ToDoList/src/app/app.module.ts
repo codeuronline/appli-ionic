79bd42fc32b480b2cee22e8c4572595524cacbbd
PCGameJK <j.kasperski@codeur.online>
2022-07-09 20:12:12 +0200

    firebase

--- ToDoList/src/app/app.module.ts
+++ ToDoList/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { AngularFireModule} from '@angular/fire/compat'
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -6,11 +8,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyAnTgbXLF9OMvNoV_wbnguvVnx2T-e3c2o",
+  authDomain: "ionic-appli-project.firebaseapp.com",
+  projectId: "ionic-appli-project",
+  storageBucket: "ionic-appli-project.appspot.com",
+  messagingSenderId: "386540319946",
+  appId: "1:386540319946:web:d6e5d3e9f443edbb53b033"
+};
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+  AngularFireModule.initializeApp(firebaseConfig),AngularFireDatabaseModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
+
+  
+
+
--- ToDoList/src/app/home/home.page.ts
+++ ToDoList/src/app/home/home.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  currentDate: string;
+  
 
-  constructor() {}
+  constructor(public afDB: AngularFireDatabase) {
+    const timeformat: Intl.DateTimeFormatOptions = {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour12: false
+  };
+
+  this.currentDate = new Date().toLocaleTimeString('fr-FR', timeformat); }
 
 }
