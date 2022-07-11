2e6e30a97ac569610e02c98837868b7fa6c7a9a7
codeuronline <j.kasperski@codeur.online>
2022-07-11 14:04:02 +0200

    update formulaire

---
+++ ionicfoundlost/foundlost/src/app/api/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  // On définit une propriété de type HttpHeaders qui représente les options de configuration
+  // d’un entête d'une requête http (la requête vers le serveur)
+  headers: HttpHeaders;
+  
+  constructor(public http: HttpClient) {
+    // On instancie le service
+    this.headers = new HttpHeaders();
+    // On définit les options de configuration et la CORS policy
+    this.headers.append('Accept', 'application/json');
+    this.headers.append('Content-Type', 'application/json');
+    this.headers.append('Access-Control-Allow-Origin', '*');
+    this.headers.append('Access-Control-Allow-Headers', 'Content-Type');
+  }
+  // On définit la méthode qui envoie les données du formulaire au serveur backend.
+  submitForm(data) {
+    return this.http.post('http://localhost/ionicserver/manage-data.php?key=create', data);
+  }
+}
+
--- ionicfoundlost/foundlost/src/app/lost/lost.page.ts
+++ ionicfoundlost/foundlost/src/app/lost/lost.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../api/user.service';
+import { FormGroup,FormBuilder ,Validators } from '@angular/forms';
 @Component({
   selector: 'app-lost',
   templateUrl: './lost.page.html',
   styleUrls: ['./lost.page.scss'],
 })
 export class LostPage implements OnInit {
-
-  constructor() { }
+  ionicForm: FormGroup;
+  defaultDate: "2022-07-11";
+  constructor(public apiService:UserService,public formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
+  getDate(e) {
+    let date = new Date(e.target.value).toISOString().substring(0, 10);
+    this.ionicForm.get('date').setValue(date, { onlyself: true});
+  }
+  submitForm() {
+    let data = {
+      description: this.ionicForm.get("description") ,
+      status: 0,
+      date: this.ionicForm.get("date"),
+      firstname: this.ionicForm.get("firstname"),
+      lastname: this.ionicForm.get("lastname"),
+      email: this.ionicForm.get(""),
 
+    }
+    console.log(this.ionicForm.value);
+    // this.apiService.submitForm(data).subscribe((res) => {
+    //   console.log("SUCCES ===", res);
+    // }
+    // )
+  }
 }
